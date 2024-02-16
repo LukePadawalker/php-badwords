122be321d2e7e2b82db5a97e856d64d875a2dcb3
LukePadawalker <padurariu.luca@gmail.com>
2024-02-16 19:09:45 +0100

    index.php done

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <title>Form di censura</title>
 </head>
 <body>
+<form action="process_request.php" method="post">
+    <label for="paragrafo">Inserisci il paragrafo:</label><br>
+    <input type="text">
+    <label for="parola">Inserisci la parola da censurare:</label><br>
+    <input type="text" id="parola"><br><br>
+
+    <input type="submit" value="Invia">
+</form>
 
 
 </body>
